97600bef30fd893968c15b66e7ebcc803522605b
JasonSkillman <jason_skillman@outlook.com>
2020-10-04 13:12:25 -0400

    Created load scene command

---
+++ Assets/Command-Console/Scripts/Commands/LoadSceneCommand.cs
@@ -0,0 +1,26 @@
+﻿using DebugCommandConsole;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneCommand : ICommand {
+    public void Action(string[] args) {
+        string sceneName = args[0];
+
+        //Load the scene if it exists
+        if(Application.CanStreamedLevelBeLoaded(sceneName)) {
+            SceneManager.LoadScene(sceneName);
+            CommandConsole.Instance.Log($"Loading scene {sceneName}");
+        } else {
+            CommandConsole.Instance.LogError($"Scene {sceneName} does not exist");
+        }
+    }
+
+    public string[] SuggestedArgs(string[] args) {
+        return new[] { 
+            "<scene-name>" 
+        };
+    }
+
+    public string Label => "load-scene";
+}
