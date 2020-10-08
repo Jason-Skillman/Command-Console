f047219878f41f8b13d8db1f342b458d4405d62e
JasonSkillman <jason_skillman@outlook.com>
2020-10-08 11:39:30 -0400

    Added editor to create the console from menu

---
+++ Editor/CommandConsoleEditor.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework.Internal;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CommandConsoleEditor : MonoBehaviour {
+
+    [MenuItem("GameObject/Console/CommandConsole", false, 10)]
+    static void CreateCustomPrimitiveGameObject(MenuCommand menuCommand) {
+        //Use the asset database to fetch the console prefab
+        GameObject consolePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(
+            "Packages/com.jasonskillman.commandconsole/Runtime/Prefabs/CommandConsole.prefab");
+
+        //Instantiate the prefab in the hierarchy
+        PrefabUtility.InstantiatePrefab(consolePrefab);
+
+        Selection.activeObject = consolePrefab;
+    }
+
+}
