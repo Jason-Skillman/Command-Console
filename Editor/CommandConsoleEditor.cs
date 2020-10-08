7108ad608bc0ae1f764224ba6cc9735c6f43f194
JasonSkillman <jason_skillman@outlook.com>
2020-10-08 11:45:37 -0400

    Cant create multiple consoles

--- Editor/CommandConsoleEditor.cs
+++ Editor/CommandConsoleEditor.cs
@@ -1,13 +1,23 @@
-﻿using NUnit.Framework.Internal;
+﻿using DebugCommandConsole;
+using NUnit.Framework.Internal;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public class CommandConsoleEditor : MonoBehaviour {
+public class CommandConsoleEditor {
 
     [MenuItem("GameObject/Console/CommandConsole", false, 10)]
     static void CreateCustomPrimitiveGameObject(MenuCommand menuCommand) {
+        //Check if the console has already been created
+        CommandConsole existingConsole = GameObject.FindObjectOfType<CommandConsole>();
+
+        if(existingConsole != null) {
+            Debug.LogWarning("Command console has already been created.");
+            Selection.activeObject = existingConsole;
+            return;
+        }
+
         //Use the asset database to fetch the console prefab
         GameObject consolePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(
             "Packages/com.jasonskillman.commandconsole/Runtime/Prefabs/CommandConsole.prefab");
